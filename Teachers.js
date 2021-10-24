656442c359ac78a392decb2631b9a55b127bc340
Arthur Borges <arthurborges.dev@gmail.com>
2021-10-24 18:36:33 -0300

    Render mockup teacher data uusing a map

--- Teachers.js
+++ Teachers.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const Teachers = () => (
-  <View>
-    <Text>
-      Teacher 1
-    </Text>
-    <Text>
-      Teacher 2
-    </Text>
-    <Text>
-      Teacher 3
-    </Text>
-    <Text>
-      Teacher 4
-    </Text>
-    <Text>
-      Teacher 5
-    </Text>
-  </View>
-);
+const Teachers = () => {
+  const data = [
+    {
+      id: 1,
+      name: 'Arthur',
+      details: 'I like teaching',
+      subject: 'React'
+    },
+    {
+      id: 2,
+      name: 'Fernando',
+      details: 'I study hard and teach harder!',
+      subject: 'Rails'
+    },
+    {
+      id: 3,
+      name: 'Andressa',
+      details: "I'm good ad marketing",
+      subject: 'CSS'
+    },
+  ];
+  return (
+    <View>
+      {
+        data.map((teacher) => ( 
+          <View key={teacher.id}>
+            <Text>Name: {teacher.name}</Text>
+            <Text>Details: {teacher.details}</Text>
+            <Text>Subject: {teacher.subject}</Text>
+          </View>
+        ))
+      }
+    </View>
+  );
+};
 
 export default Teachers;
