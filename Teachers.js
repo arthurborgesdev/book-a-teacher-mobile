008136d182de96577b1edf089bd2977d77cc5cb7
Arthur Borges <arthurborges.dev@gmail.com>
2021-10-24 18:56:36 -0300

    Style the View and Text for teachers cards

--- Teachers.js
+++ Teachers.js
@@ -18,18 +18,24 @@ const Teachers = () => {
     {
       id: 3,
       name: 'Andressa',
-      details: "I'm good ad marketing",
-      subject: 'CSS'
+      details: "I'm good at marketing",
+      subject: 'UX Writter'
     },
   ];
   return (
     <View>
       {
         data.map((teacher) => ( 
-          <View key={teacher.id}>
-            <Text>Name: {teacher.name}</Text>
-            <Text>Details: {teacher.details}</Text>
-            <Text>Subject: {teacher.subject}</Text>
+          <View 
+            key={teacher.id} 
+            style={{
+              backgroundColor: 'green', 
+              padding: 20,
+              margin: 10,
+            }}>
+            <Text style={{fontSize: 20}}>Name: {teacher.name}</Text>
+            <Text style={{fontSize: 20}}>Details: {teacher.details}</Text>
+            <Text style={{fontSize: 20}}>Subject: {teacher.subject}</Text>
           </View>
         ))
       }
