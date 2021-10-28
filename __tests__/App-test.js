376d06d8a28891ce989da9075ea8d2e3a0fac021
Arthur Borges <arthurborges.dev@gmail.com>
2021-10-28 18:03:12 -0300

    Practice testing render components byy renderin FlatListBasics and PizzaTranslator components

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,6 +5,8 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import FlatListBasics from '../FlatListBasics';
+import PizzaTranslator from '../PizzaTranslator';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
@@ -13,4 +15,12 @@ jest.useFakeTimers();
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+it('renders the FlatListBasics correctly', () => {
+  renderer.create(<FlatListBasics />);
+});
+
+it('renders the PizzaTranslator correctly', () => {
+  renderer.create(<PizzaTranslator />);
 });
